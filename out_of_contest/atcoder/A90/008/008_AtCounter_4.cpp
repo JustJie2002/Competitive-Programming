2204507f5c7d8bbedddc5a0094ff1c3d3ff58647
Jie Chen <jiechen067@gmail.com>
2024-04-04 20:54:03 -0400

    Fix translation

---
+++ out_of_contest/atcoder/A90/008/008_AtCounter_4.cpp
@@ -0,0 +1,207 @@
+/***************************************************************************************************
+ * author : Jie Chen (3rd Year CS)
+ * school : Rochester Institute of Technology
+ * created: 04.04.2024 17:51:59
+****************************************************************************************************/
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// http://www.open-std.org/jtc1/sc22/wg21/docs/papers/2016/p0200r0.html
+template<class Fun> class y_combinator_result {
+    Fun fun_;
+public:
+    template<class T> explicit y_combinator_result(T &&fun): fun_(std::forward<T>(fun)) {}
+    template<class ...Args> decltype(auto) operator()(Args &&...args) { return fun_(std::ref(*this), std::forward<Args>(args)...); }
+};
+template<class Fun> decltype(auto) y_combinator(Fun &&fun) { return y_combinator_result<std::decay_t<Fun>>(std::forward<Fun>(fun)); }
+
+#ifdef BROKEN_CODE
+#include <bits/debug.h>
+#else
+#define dbg(...) 10082002
+#define dbp(...) "Need Internship"
+#endif
+
+using i64 = long long;
+
+// Credit: Jiangly
+template<class T>
+constexpr T power(T a, i64 b) {
+    T res = 1;
+    for (; b; b /= 2, a *= a) {
+        if (b % 2) {
+            res *= a;
+        }
+    }
+    return res;
+}
+
+constexpr i64 mul(i64 a, i64 b, i64 p) {
+    i64 res = a * b - i64(1.L * a * b / p) * p;
+    res %= p;
+    if (res < 0) {
+        res += p;
+    }
+    return res;
+}
+template<int P>
+struct MInt {
+    int x;
+    constexpr MInt() : x{} {}
+    constexpr MInt(i64 x) : x{norm(x % getMod())} {}
+    
+    static int Mod;
+    constexpr static int getMod() {
+        if (P > 0) {
+            return P;
+        } else {
+            return Mod;
+        }
+    }
+    constexpr static void setMod(int Mod_) {
+        Mod = Mod_;
+    }
+    constexpr int norm(int x) const {
+        if (x < 0) {
+            x += getMod();
+        }
+        if (x >= getMod()) {
+            x -= getMod();
+        }
+        return x;
+    }
+    constexpr int val() const {
+        return x;
+    }
+    explicit constexpr operator int() const {
+        return x;
+    }
+    constexpr MInt operator-() const {
+        MInt res;
+        res.x = norm(getMod() - x);
+        return res;
+    }
+    constexpr MInt inv() const {
+        assert(x != 0);
+        return power(*this, getMod() - 2);
+    }
+    constexpr MInt &operator*=(MInt rhs) & {
+        x = 1LL * x * rhs.x % getMod();
+        return *this;
+    }
+    constexpr MInt &operator+=(MInt rhs) & {
+        x = norm(x + rhs.x);
+        return *this;
+    }
+    constexpr MInt &operator-=(MInt rhs) & {
+        x = norm(x - rhs.x);
+        return *this;
+    }
+    constexpr MInt &operator/=(MInt rhs) & {
+        return *this *= rhs.inv();
+    }
+    friend constexpr MInt operator*(MInt lhs, MInt rhs) {
+        MInt res = lhs;
+        res *= rhs;
+        return res;
+    }
+    friend constexpr MInt operator+(MInt lhs, MInt rhs) {
+        MInt res = lhs;
+        res += rhs;
+        return res;
+    }
+    friend constexpr MInt operator-(MInt lhs, MInt rhs) {
+        MInt res = lhs;
+        res -= rhs;
+        return res;
+    }
+    friend constexpr MInt operator/(MInt lhs, MInt rhs) {
+        MInt res = lhs;
+        res /= rhs;
+        return res;
+    }
+    friend constexpr std::istream &operator>>(std::istream &is, MInt &a) {
+        i64 v;
+        is >> v;
+        a = MInt(v);
+        return is;
+    }
+    friend constexpr std::ostream &operator<<(std::ostream &os, const MInt &a) {
+        return os << a.val();
+    }
+    friend constexpr bool operator==(MInt lhs, MInt rhs) {
+        return lhs.val() == rhs.val();
+    }
+    friend constexpr bool operator!=(MInt lhs, MInt rhs) {
+        return lhs.val() != rhs.val();
+    }
+};
+ 
+template<>
+int MInt<0>::Mod = 998'244'353;
+ 
+template<int V, int P>
+constexpr MInt<P> CInv = MInt<P>(V).inv();
+ 
+constexpr int P = 1'000'000'007; // 998'244'353;
+using Z = MInt<P>;
+
+constexpr int BUFSIZE = 2e6 + 1;
+char buf[BUFSIZE];
+struct Reader {
+    static string nxt() {
+        scanf("%s", buf);
+        return buf;
+    }
+    static char cxt() {
+        scanf("%s", buf);
+        return buf[0];
+    }
+    static string lxt() {
+        char c;
+        string sb;
+        while (c = getchar()) {
+            if (c == '\n' || c == EOF) {
+                break;
+            }
+            sb += c;
+        }
+        return sb;
+    }
+} fs;
+
+const string S = "atcoder";
+constexpr int M = 7;
+
+Z dp[M];
+
+int get(char c) {
+    auto it = find(S.begin(), S.end(), c);
+    if (it == S.end()) {
+        return -1;
+    }
+    return int(it - S.begin());
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    auto s = fs.nxt();
+
+    for (char c : s) {
+        int o = get(c);
+        if (o == -1) {
+            continue;
+        }
+        if (o == 0) {
+            dp[o] += 1;
+        } else {
+            dp[o] += dp[o - 1];
+        }
+    }
+
+    printf("%d\n", dp[M - 1].val());
+}
+
+// ~ Just Jie
